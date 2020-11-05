84f891a8a06b8140c22b6bf602ba240f854f4ae1
dsublett <dsublett@clearvalueconsulting.com>
2020-11-05 15:43:41 -0700

    Dynamic lib

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
     products: [
         .library(
             name: "PMKCoreLocation",
+            type: .dynamic,
             targets: ["PMKCoreLocation"]),
     ],
     dependencies: [
